dbfc26aad67e4ac3906a153709b06d809253f32d
LeonardoTredese <leonardo.tredese@hotmail.com>
2024-05-24 19:48:11 +0200

    added POD

---
+++ POD.py
@@ -0,0 +1,66 @@
+import numpy as np
+import GeDiM4Py as gedim
+from functools import partial
+from weakforms import *
+from FOM import newton_solver
+
+
+def pod_base(lib, problem_data, mus, retained_energy=0.99, max_n=20):
+    inner_product, _ = gedim.AssembleStiffnessMatrix(ones, problem_data, lib)
+    fom_solver = partial(newton_solver, lib, problem_data, forcing_term)
+    snapshots = np.stack([fom_solver(mu)[0] for mu in mus])
+    e_vals, e_vecs = np.linalg.eig(snapshots @ inner_product @ snapshots.T)
+    assert np.all(
+        np.isclose(e_vals.imag, 0)
+    ), "Eigen values have non zero imaginary part"
+    cs = np.cumsum(e_vals.real)
+    N = min(np.argmax((cs / cs[-1]) > retained_energy) + 1, max_n)
+    basis = snapshots.T @ e_vecs.real[:, :N]
+    norm = np.sqrt(np.diag(basis.T @ inner_product @ basis))
+    return basis / norm
+
+
+def newton_solver_pod(
+    lib, problem_data, forcing_term, basis, mu, max_iterations=10, tol=1e-6
+):
+    diff_c = partial(newton_diffusion_coefficient, mu=mu)
+    reac_c = partial(newton_reaction_coefficient, mu=mu)
+    nl_reac_t = partial(newton_nonlinear_reaction_term, mu=mu)
+    forc_t = partial(forcing_term, mu=mu)
+    nl_forc_c = partial(newton_nonlinear_forcing_coefficient, mu=mu)
+    nl_forc_t = partial(newton_nonlinear_forcing_term, mu=mu)
+    nl_forc_d = partial(newton_nonlinear_forcing_derivative, mu=mu)
+
+    u_kn = np.zeros(basis.shape[1], order="F")
+    u_k = basis @ u_kn
+    u_strong = np.zeros(problem_data["NumberStrongs"], order="F")
+    k = 0
+    residual_norm = 1.0
+    solution_norm = 10 * tol * residual_norm
+    stiff_m, _ = gedim.AssembleStiffnessMatrix(diff_c, problem_data, lib)
+    forc_m = gedim.AssembleForcingTerm(forc_t, problem_data, lib)
+    stiff_m = basis.T @ stiff_m @ basis
+    forc_m = basis.T @ forc_m
+    while k < max_iterations and residual_norm > tol * solution_norm:
+        u_k = basis @ u_kn
+        react_m, _ = gedim.AssembleNonLinearReactionMatrix(
+            reac_c, nl_reac_t, u_k, u_strong, problem_data, lib
+        )
+        nl_forc_m = gedim.AssembleNonLinearForcingTerm(
+            nl_forc_c, nl_forc_t, u_k, u_strong, problem_data, lib
+        )
+        nl_forc_d_m = gedim.AssembleNonLinearDerivativeForcingTerm(
+            ones_derivative, nl_forc_d, u_k, u_strong, problem_data, lib
+        )
+
+        du = gedim.LUSolver(
+            stiff_m + basis.T @ react_m @ basis,
+            forc_m - basis.T @ (nl_forc_m - nl_forc_d_m),
+            lib,
+        )
+        u_kn = u_kn + du
+        u_k = basis @ u_kn
+        solution_norm = gedim.ComputeErrorL2(zeros, u_k, u_strong, lib)
+        residual_norm = gedim.ComputeErrorL2(zeros, basis @ du, u_strong, lib)
+        k = k + 1
+    return u_k, u_strong, residual_norm / solution_norm, k
--- main.py
+++ main.py
@@ -1,25 +1,47 @@
 import GeDiM4Py as gedim
 import numpy as np
-from weakforms import test_forcing_term, forcing_term
+from weakforms import test_forcing_term, forcing_term, ones
 from FOM import newton_solver
+from POD import pod_base, newton_solver_pod
 
 # setup lib
-lib = gedim.ImportLibrary('/content/CppToPython/release/GeDiM4Py.so')
-gedim.Initialize({'GeometricTolerance': 1.0e-8}, lib)
+lib = gedim.ImportLibrary("/content/CppToPython/release/GeDiM4Py.so")
+gedim.Initialize({"GeometricTolerance": 1.0e-8}, lib)
 
 # setup problem
-mu = np.array([3.34, 4.45])
-mesh_size = 1e-3
+mu = np.array([0.34, 0.45])
+mesh_size = 1e-2
 order = 1
-domain = {'SquareEdge': 4.0, 'VerticesBoundaryCondition': [1, 1, 1, 1], 'EdgesBoundaryCondition': [
-    1, 1, 1, 1], 'DiscretizationType': 1, 'MeshCellsMaximumArea': mesh_size}
+domain = {
+    "SquareEdge": 1.0,
+    "VerticesBoundaryCondition": [1, 1, 1, 1],
+    "EdgesBoundaryCondition": [1, 1, 1, 1],
+    "DiscretizationType": 1,
+    "MeshCellsMaximumArea": mesh_size,
+}
 _, mesh = gedim.CreateDomainSquare(domain, lib)
 
-discreteSpace = {'Order': order, 'Type': 1, 'BoundaryConditionsType': [1, 2]}
+discreteSpace = {"Order": order, "Type": 1, "BoundaryConditionsType": [1, 2]}
 problem_data, dofs, strongs = gedim.Discretize(discreteSpace, lib)
 
-u, u_strong, relative_error, k = newton_solver(
-    lib, problem_data, forcing_term, mu)
+
+# Solve FOM
+u, u_strong, relative_error, k = newton_solver(lib, problem_data, forcing_term, mu)
 
 print(f"Iterations: {k} Relative err: {relative_error}")
 gedim.PlotSolution(mesh, dofs, strongs, u, u_strong)
+
+# Extract Base
+train_set = np.random.uniform(0.1, 1, size=(100, 2))
+basis = pod_base(lib, problem_data, train_set)
+print(f"found basis of dimensions {basis.shape}")
+
+u_pod, u_pod_strong, _, _ = newton_solver_pod(
+    lib, problem_data, forcing_term, basis, mu
+)
+
+error = u - u_pod
+inner_product, _ = gedim.AssembleStiffnessMatrix(ones, problem_data, lib)
+error_norm = np.sqrt(np.abs(error.T @ inner_product @ error))
+fom_norm = np.sqrt(np.abs(u.T @ inner_product @ u))
+print(f"POD Relative Error {error_norm / fom_norm}")
